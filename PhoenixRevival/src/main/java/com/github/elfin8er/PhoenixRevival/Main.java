c471ca5a8de74c35ab8bebc6c091e78fee052cfd
elfin8er <elfin8er@gmail.com>
2015-05-02 23:54:40 -0400

    Basic Command

--- PhoenixRevival/src/main/java/com/github/elfin8er/PhoenixRevival/Main.java
+++ PhoenixRevival/src/main/java/com/github/elfin8er/PhoenixRevival/Main.java
@@ -1,5 +1,25 @@
 package com.github.elfin8er.PhoenixRevival;
 
+import org.slf4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.util.command.spec.CommandSpec;
+
+import com.google.inject.Inject;
+
+@Plugin(name = "PhoenixRevival", id = "PhoenixRevival", version = "ALPHA")
 public class Main {
+	
+	@Inject
+	Game game;
+	
+	@Inject
+	Logger logger;
+	
+	CommandSpec basicCommand = CommandSpec.builder()
+			.setDescription(Texts.of("Short Command Description"))
+			.setExecutor(new BlahCommand())
+			.build();
 
 }
