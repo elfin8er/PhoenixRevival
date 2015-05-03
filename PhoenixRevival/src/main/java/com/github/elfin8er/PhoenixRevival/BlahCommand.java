8fd14b7d8595d0731c9f512cc83ada323e59db2b
elfin8er <elfin8er@gmail.com>
2015-05-02 23:59:25 -0400

    Fixed

---
+++ PhoenixRevival/src/main/java/com/github/elfin8er/PhoenixRevival/BlahCommand.java
@@ -0,0 +1,15 @@
+package com.github.elfin8er.PhoenixRevival;
+
+import org.spongepowered.api.util.command.CommandException;
+import org.spongepowered.api.util.command.CommandResult;
+import org.spongepowered.api.util.command.CommandSource;
+import org.spongepowered.api.util.command.args.CommandContext;
+import org.spongepowered.api.util.command.spec.CommandExecutor;
+
+public class BlahCommand implements CommandExecutor {
+
+	public CommandResult execute(CommandSource sender, CommandContext arguments) throws CommandException {
+		return null;
+	}
+
+}
--- PhoenixRevival/src/main/java/com/github/elfin8er/PhoenixRevival/Main.java
+++ PhoenixRevival/src/main/java/com/github/elfin8er/PhoenixRevival/Main.java
@@ -2,6 +2,8 @@ package com.github.elfin8er.PhoenixRevival;
 
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
+import org.spongepowered.api.event.Subscribe;
+import org.spongepowered.api.event.state.InitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.text.Texts;
 import org.spongepowered.api.util.command.spec.CommandSpec;
@@ -17,9 +19,15 @@ public class Main {
 	@Inject
 	Logger logger;
 	
-	CommandSpec basicCommand = CommandSpec.builder()
+	@Subscribe
+	public void onServerInitialization(InitializationEvent event){
+		CommandSpec basicCommand = CommandSpec.builder()
 			.setDescription(Texts.of("Short Command Description"))
 			.setExecutor(new BlahCommand())
 			.build();
+		
+		game.getCommandDispatcher().register(this, basicCommand, "command");
+	}
+	
 
 }
