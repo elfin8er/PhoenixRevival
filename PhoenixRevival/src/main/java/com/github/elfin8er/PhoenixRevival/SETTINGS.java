0d36964b5ba2de74bcdeaad69d856ec6fcafc178
elfin8er <elfin8er@gmail.com>
2015-05-03 00:44:38 -0400

    Created PhoenixPlayer object

--- PhoenixRevival/src/main/java/com/github/elfin8er/PhoenixRevival/SETTINGS.java
+++ PhoenixRevival/src/main/java/com/github/elfin8er/PhoenixRevival/SETTINGS.java
@@ -2,7 +2,7 @@ package com.github.elfin8er.PhoenixRevival;
 
 public class SETTINGS {
 
-	public final static int StartingMoney = 100;
+	public final static Double StartingMoney = 100.00;
 	public final static String CurrencySymbol = "₱";
 	
 }
