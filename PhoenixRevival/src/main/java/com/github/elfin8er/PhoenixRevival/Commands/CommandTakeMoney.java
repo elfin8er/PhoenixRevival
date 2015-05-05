165a9e5672c760edcded2e6ef3d15596d4875321
LiruJ <liruotaku@googlemail.com>
2015-05-05 04:25:50 +0100

    t

---
+++ PhoenixRevival/src/main/java/com/github/elfin8er/PhoenixRevival/Commands/CommandTakeMoney.java
@@ -0,0 +1,40 @@
+package com.github.elfin8er.PhoenixRevival.Commands;
+
+import org.spongepowered.api.entity.player.Player;
+import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.util.command.CommandException;
+import org.spongepowered.api.util.command.CommandResult;
+import org.spongepowered.api.util.command.CommandSource;
+import org.spongepowered.api.util.command.args.CommandContext;
+import org.spongepowered.api.util.command.spec.CommandExecutor;
+
+import com.github.elfin8er.PhoenixRevival.Phoenix;
+import com.github.elfin8er.PhoenixRevival.SETTINGS;
+
+public class CommandTakeMoney implements CommandExecutor {
+
+	private Phoenix plugin;
+	
+	public CommandTakeMoney(Phoenix plugin) {
+        this.plugin = plugin;
+	}
+	
+	public CommandResult execute(CommandSource taker, CommandContext arguments) throws CommandException {
+		
+		String target = (String) arguments.getOne("player").orNull();
+		int amount = (int) arguments.getOne("amt").orNull();
+		
+		//checks to make sure it's not negative
+		if (amount <= 0){
+			return CommandResult.empty();
+		}
+		
+		//gives money
+		this.plugin.giveMoney(target, (Player) taker, amount);
+		
+		//tells sender that the money has been sent
+		taker.sendMessage(Texts.of("Amount of " + amount + SETTINGS.CurrencySymbol +  " Taken from " + target + "."));
+
+		return CommandResult.success();
+	}
+}
